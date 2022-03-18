0a90d190f5ea50b268e38c4bdcfa6b77970436e7
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-03-18 15:06:21 -0400

    refactor smile.py

--- smile_detection/homework1_smile_sbuasai2.py
+++ smile_detection/homework1_smile_sbuasai2.py
@@ -12,28 +12,14 @@ def fPC (y, yhat):
 # Argument: Set of tuples of (r1,c1,r2,c2), 3D np.array() of images, the np.array() of ground truth
 # Return: the percent correct given the set of predictors
 def measureAccuracyOfPredictors (predictors, X, y):
-    yhat = np.array([])
-    i = 1
-    for image in X:
-        i += 1
-        # add the result (1,0) onto yhat array
-        yhat = np.append(yhat, [predictSmile(predictors, image)])
-    # return the fPC of our guesses based on the current set of predictors 
-    return fPC(y, yhat)
+    yhat = np.zeros_like(y)
+    for i, p in enumerate(predictors):
+        r1,c1,r2,c2 = p
+        for j in range(len(X)):
+            yhat[j] += 1 if X[j,r1,c1] > X[j,r2,c2] else 0
 
-## Predict whether an image is smiling or not based on the set of predictors
-# Argument: Set of tuples of (r1,c1,r2,c2), 2D np.array() of an image
-# Return: 1 if the image is a smile, 0 otherwise
-def predictSmile (predictors, image):
-    prediction = np.array([])
-    for p in predictors:
-        # declare the pixel pairs (r1,c1), (r2,c2)
-        r1,c1,r2,c2 = p[0],p[1],p[2],p[3]
-        # if the first pixel is brighter than the second pixel, then we say the prediction_result is 1
-        p_res = 1 if image[r1,c1] > image[r2,c2] else 0
-        prediction = np.append(prediction, [p_res])
-    # return that image is smiling if the predictor set get more
-    return 1 if np.mean(prediction) > 0.5 else 0
+    yhat = np.where(yhat/len(predictors) > 0.5, 1, 0)
+    return fPC(y, yhat)
 
 ## step-wise regression building on 6 rounds of predictors
 # Argument: 3D np.array() of training faces, 1D np.array() of training labels, 3D np.array() of testing faces, 1D np.array() of testing labels
@@ -66,10 +52,10 @@ def loadData (which):
     labels = np.load("{}ingLabels.npy".format(which))
     return faces, labels
 
-## Same functions as findPredictors but vectorize the coordinates instead of using for loops
+## Find 6 pairs of predictors that best predict the training images
 # Argument: 3D np.array() of faces, 1D np.array() of labels
 # Return: the fPC and the set of predictors
-def findPredictors2 (Faces, Labels):
+def findPredictors (Faces, Labels):
     # initialize the final sets of predictors and the preditions from previous set or preditors
     final_phi = set()
     prev_guess = np.zeros_like(Labels)
@@ -106,68 +92,10 @@ def findPredictors2 (Faces, Labels):
 
     return max_fPC, final_phi
 
-## Given a set of predictors from the training set, find the fPC of the predictors on the testing set
-# Argument: set of predictor tuples,  3D np.array() of faces, 1D np.array() of labels
-def testPredictors (predictors, Faces, Labels):
-    guess = np.zeros_like(Labels)
-    for i, p in enumerate(predictors):
-        r1,c1,r2,c2 = p
-        for j in range(len(Faces)):
-            guess[j] = 1 if Faces[j,r1,c1] > Faces[j,r2,c2] else 0
-
-    guess = np.where(guess/len(predictors) > 0.5, 1, 0)
-    return fPC(Labels, guess)
-
-## Find all possible predictors
-# Argument: 3D np.array() of faces, 1D np.array() of labels
-# Return: the fPC and the set of predictors
-def findPredictors (Faces, Labels):
-    """
-    - Loop 6 times to get 6 predictors
-      - In the first loop:
-        - Initiate a current best fPC and current best predictor.
-        - Choose a pair of pixels to compare {r1,c1}.
-        - Loop through all the training data to and get the fPC of the predictor.
-        - Store the fPC and the predictor if it is the current best.
-        - After the loop, get the current best predictor.
-      - In the following loops:
-        - Initiate a current best fPC and current best predictor.
-        - Choose a pair of pixels to compare {r1,c1,r2,c2}.
-        - Loop through all the training data and get the fPC of the predictor ALONG WITH THE PREVIOUSLY CHOSEN PREDICTOR.
-        - Store the fPC and the predictor if it is the current best.
-        - After the loop, get the current best predictor.
-    """
-    final_phi = set()
-    for i in range(1):
-        max_fPC = 0
-        max_phi = (0,0,0,0)
-        for r1,c1 in np.ndindex((24,24)):
-            for r2,c2 in np.ndindex((24,24)):
-                curr_phi = (r1,c1,r2,c2)
-                if (r1 == r2 and c1 == c2) or curr_phi in final_phi:
-                    continue
-                else:
-                    curr_fPC =  measureAccuracyOfPredictors(final_phi | {(r1,c1,r2,c2)}, trainingFaces[:10], trainingLabels[:10])
-                    if curr_fPC > max_fPC:
-                        max_fPC = curr_fPC
-                        max_phi = curr_phi
-
-        final_phi.add(max_phi)
-
-    return max_fPC, final_phi
-
 if __name__ == "__main__":
     testingFaces, testingLabels = loadData("test")
     trainingFaces, trainingLabels = loadData("train")
-    print(fPC(np.array([1,1,1,1,1]),np.array([1,1,1,2,2])))
-    print(len(testingFaces))
-    print(trainingLabels)
-    print(np.mean(np.array([1,1,0])))
-    print(predictSmile({(0,0,0,1)},trainingFaces[0]))
-    print(measureAccuracyOfPredictors({(0,0,0,1)}, trainingFaces, trainingLabels))
-    # res, pred = findPredictors(trainingFaces, trainingLabels)
-    # res, pred = findPredictors2(trainingFaces, trainingLabels)
-    # print(res, pred)
-    # stepwiseRegression(trainingFaces, trainingLabels, testingFaces, testingLabels, pred)
-    pred = {(19, 11, 14, 7), (12, 5, 10, 13), (11, 19, 12, 12), (14, 5, 16, 6), (20, 7, 17, 7), (20, 17, 16, 17)}
-    print(testPredictors(pred, testingFaces, testingLabels))
+    res, pred = findPredictors(trainingFaces, trainingLabels)
+    print(res, pred)
+    print(measureAccuracyOfPredictors(pred, testingFaces, testingLabels))
+    stepwiseRegression(trainingFaces, trainingLabels, testingFaces, testingLabels, pred)
