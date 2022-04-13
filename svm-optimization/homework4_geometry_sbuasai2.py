53839b911cefc2b7f16410ce005645f233ce164c
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-04-12 21:55:04 -0400

    add find equation and find weight functions and print out margin

--- svm-optimization/homework4_geometry_sbuasai2.py
+++ svm-optimization/homework4_geometry_sbuasai2.py
@@ -2,9 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from homework4_sbuasai2 import SVM4342
 
+## Convert normal vector to slope of y = mx + b
 def normalToSlope (normal_vector):
     x,y = normal_vector
     return -x/y
+
+## Given an arbitrary normal vector and some data points,
+#  find the normal weight vector w and bias b
+def findWeight (normal, coord_up, coord_down):
+    n_1, n_2 = normal
+    x_1, y_1 = coord_up
+    x_2, y_2 = coord_down
+    # Equation for upperbound: x_1*(m*n_1) + y_1*(m*n_2) + b - 1 = 0
+    # Equation for lowerbound: x_2*(m*n_1) + y_2*(m*n_2) + b + 1 = 0
+    # Solve for m and b given the two equations
+    m = 2/(n_1*(x_1-x_2) + n_2*(y_1-y_2))
+    b = 1 - x_1*m*n_1 - y_1*m*n_2
+    # normal weight vector w is m multipled of normal vector (w = m*n)
+    w = np.array([n_1,n_2])*m
+    return w, b
+
+## Given x, w and b, find the equation y = mx + b
+def findEquation (x, w, b):
+    w_1, w_2 = w
+    # Equation: w_1*x + w_2*y + b = 0,1,-1
+    # Solve for y given the equation above
+    y = (-w_1*x - b) / w_2
+    y_up = (-w_1*x - b + 1) / w_2
+    y_down = (-w_1*x - b - 1) / w_2
+    return y, y_up, y_down
 
 X = np.load("hw4_X.npy")
 y = np.load("hw4_y.npy")
@@ -15,26 +41,18 @@ plt.scatter(X[0,0:n], X[1,0:n])
 plt.scatter(X[0,n:], X[1,n:])
 
 # H1
-m1 = 2/3
-b1 = 1
-w1 = np.array([0,1])*m1
+w1, b1 = findWeight(normal=(0,1), coord_up=(0,0), coord_down=(-6,-3))
 slope1 = normalToSlope(w1)
-y1 = 0*x-3/2
-y_up1 = 0*x+0
-y_down1 = 0*x-3
+y1, y_up1, y_down1 = findEquation(x, w1, b1)
 
 plt.plot(x, y1, 'k-')
 plt.plot(x, y_up1, 'k--')
 plt.plot(x, y_down1, 'k:')
 
 # H2
-m2 = 2.85714
-b2 = 2.42857
-w2 = np.array([-0.3,1])*m2
+w2, b2 = findWeight(normal=(-0.3,1), coord_up=(5,1), coord_down=(-6,-3))
 slope2 = normalToSlope(w2)
-y2 = 0.3*x-0.85
-y_up2 = 0.3*x-0.5
-y_down2 = 0.3*x-1.2
+y2, y_up2, y_down2 = findEquation(x, w2, b2)
 
 plt.plot(x, y2, 'b-')
 plt.plot(x, y_up2, 'b--')
@@ -46,11 +64,12 @@ Wtilde.fit(X.T, y)
 b3 = Wtilde.b
 w3 = Wtilde.w
 slope3 = normalToSlope(w3)
-y3 = -0.285714*x-2.35714
-y_up3 = -0.285714*x-(9.42857e-8)
-y_down3 = -0.285714*x-4.71429
+y3, y_up3, y_down3 = findEquation(x, w3, b3)
 
 plt.plot(x, y3, 'r-')
 plt.plot(x, y_up3, 'r--')
 plt.plot(x, y_down3, 'r:')
+print(f"Hyperplane 1 margin: {np.linalg.norm(w1)}")
+print(f"Hyperplane 2 margin: {np.linalg.norm(w2)}")
+print(f"Hyperplane 3 margin: {np.linalg.norm(w3)}")
 plt.show()
